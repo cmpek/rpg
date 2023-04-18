9dc3b52ad67daa71471abfae46c48cae60de4c33
Shvedov Andrey <shvedov.ag@lsrgroup.ru>
2023-04-18 16:44:29 +0300

    lesson10

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "map.h" //подключили код с картой
+#include "view.h"//подключили код с видом камеры
 
 using namespace sf;
 
-
-
 ////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
 class Player {
+private: float x, y;
 public:
-    float x, y, w, h, dx, dy, speed ;
-    int dir ;
+    float w, h, dx, dy, speed;
+    int dir = 0;
     String File;
     Image image;
     Texture texture;
     Sprite sprite;
     Player(String F, float X, float Y, float W, float H){
-        dx=0; dy=0; speed=0; dir = 0;
+        dx=0;dy=0;speed=0;
+
         File = F;
         w = W; h = H;
         image.loadFromFile("../assets/images/" + File);
@@ -40,24 +41,34 @@ public:
         y += dy*time;
 
         speed = 0;
-        sprite.setPosition(x,y);
+        sprite.setPosition(x, y);
     }
-};
 
+    float getplayercoordinateX(){	//этим методом будем забирать координату Х
+        return x;
+    }
+    float getplayercoordinateY(){	//этим методом будем забирать координату Y
+        return y;
+    }
+
+};
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(640, 480), "Lesson 9. kychka-pc.ru");
+    RenderWindow window(sf::VideoMode(640, 480), "Lesson 10. kychka-pc.ru");
+    view.reset(sf::FloatRect(0, 0, 640, 480));//размер "вида" камеры при создании объекта вида камеры. (потом можем менять как хотим) Что то типа инициализации.
 
-    Image map_image;//объект изображения для карты
-    map_image.loadFromFile("../assets/images/map.png");//загружаем файл для карты
-    Texture map;//текстура карты
-    map.loadFromImage(map_image);//заряжаем текстуру картинкой
-    Sprite s_map;//создаём спрайт для карты
-    s_map.setTexture(map);//заливаем текстуру спрайтом
+
+    Image map_image;
+    map_image.loadFromFile("../assets/images/map.png");
+    Texture map;
+    map.loadFromImage(map_image);
+    Sprite s_map;
+    s_map.setTexture(map);
 
 
     Player p("hero.png", 250, 250, 96.0, 96.0);
+
 
     float CurrentFrame = 0;
     Clock clock;
@@ -77,57 +88,61 @@ int main()
                 window.close();
         }
 
-
         ///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
-        if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
             p.dir = 1; p.speed = 0.1;
             CurrentFrame += 0.005*time;
             if (CurrentFrame > 3) CurrentFrame -= 3;
             p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
+            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
         }
 
-        if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
             p.dir = 0; p.speed = 0.1;
             CurrentFrame += 0.005*time;
             if (CurrentFrame > 3) CurrentFrame -= 3;
             p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
+            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
         }
 
-        if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+        if (Keyboard::isKeyPressed(Keyboard::Up)) {
             p.dir = 3; p.speed = 0.1;
             CurrentFrame += 0.005*time;
             if (CurrentFrame > 3) CurrentFrame -= 3;
             p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 288, 96, 96));
+            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
 
         }
 
-        if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) {
+        if (Keyboard::isKeyPressed(Keyboard::Down)) {
             p.dir = 2; p.speed = 0.1;
             CurrentFrame += 0.005*time;
             if (CurrentFrame > 3) CurrentFrame -= 3;
             p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
+            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
 
         }
+
         p.update(time);
-
-
+        viewmap(time);//функция скроллинга карты, передаем ей время sfml
+        changeview();//прикалываемся с камерой вида
+        window.setView(view);//"оживляем" камеру в окне sfml
         window.clear();
+
 
         /////////////////////////////Рисуем карту/////////////////////
         for (int i = 0; i < HEIGHT_MAP; i++)
             for (int j = 0; j < WIDTH_MAP; j++)
             {
-                if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32)); //если встретили символ пробел, то рисуем 1й квадратик
-                if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));//если встретили символ s, то рисуем 2й квадратик
-                if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));//если встретили символ 0, то рисуем 3й квадратик
+                if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32));
+                if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));
+                if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
 
 
-                s_map.setPosition(j * 32, i * 32);//по сути раскидывает квадратики, превращая в карту. то есть задает каждому из них позицию. если убрать, то вся карта нарисуется в одном квадрате 32*32 и мы увидим один квадрат
+                s_map.setPosition(j * 32, i * 32);
 
-                window.draw(s_map);//рисуем квадратики на экран
+                window.draw(s_map);
             }
-
-
         window.draw(p.sprite);
         window.display();
     }
