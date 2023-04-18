e3f643d843f437582632ef3eaff6de8e3976aa70
Shvedov Andrey <shvedov.ag@lsrgroup.ru>
2023-04-18 16:56:57 +0300

    lesson13

--- main.cpp
+++ main.cpp
@@ -8,17 +8,18 @@ using namespace sf;
 
 ////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
 class Player {
-private: float x, y ;
+private: float x, y;
 public:
-    float w, h, dx, dy, speed ;
-    int dir, playerScore;//новая переменная, хранящая очки игрока
+    float w, h, speed,dx,dy;
+    int dir, playerScore, health;//новая переменная, хранящая жизни игрока
+    bool life;//переменная жизнь, логическая
     String File;
     Image image;
     Texture texture;
     Sprite sprite;
     Player(String F, float X, float Y, float W, float H){
-        dir = 0; playerScore = 0;
-        dx=0;dy=0;speed=0;
+        dx=0;dy=0;dir = 0; speed = 0; playerScore = 0; health = 100; // инициализировали переменную жизни в конструкторе
+        life = true;//инициализировали логическую переменную жизни
         File = F;
         w = W; h = H;
         image.loadFromFile("../assets/images/" + File);
@@ -41,10 +42,10 @@ public:
 
         x += dx*time;
         y += dy*time;
-
         speed = 0;
         sprite.setPosition(x, y);
         interactionWithMap();
+        if (health <= 0){ life = false; }//если жизней меньше либо равно 0, то умираем
     }
 
     float getplayercoordinateX(){
@@ -87,6 +88,17 @@ public:
                     playerScore++;//если взяли камень, переменная playerScore=playerScore+1;
                     TileMap[i][j] = ' ';
                 }
+
+                if (TileMap[i][j] == 'f') {
+                    health -= 40;//если взяли ядовитейший в мире цветок,то переменная health=health-40;
+                    TileMap[i][j] = ' ';//убрали цветок
+                }
+
+                if (TileMap[i][j] == 'h') {
+                    health += 20;//если взяли сердечко,то переменная health=health+20;
+                    TileMap[i][j] = ' ';//убрали сердечко
+                }
+
             }
     }
 
@@ -94,14 +106,14 @@ public:
 
 int main()
 {
-    RenderWindow window(VideoMode(640, 480), "Lesson 12. kychka-pc.ru");
+    RenderWindow window(VideoMode(640, 480), "Lesson 13. kychka-pc.ru");
     view.reset(FloatRect(0, 0, 640, 480));
 
-    Font font;//шрифт
-    font.loadFromFile("../assets/fonts/CyrilicOld.ttf");//передаем нашему шрифту файл шрифта
-    Text text("", font, 20);//создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
-    text.setColor(Color::Red);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
-    text.setStyle(Text::Bold);//жирный текст.
+    Font font;
+    font.loadFromFile("../assets/fonts/CyrilicOld.ttf");
+    Text text("", font, 20);
+    text.setColor(Color::Red);
+    text.setStyle(Text::Bold);
 
     Image map_image;
     map_image.loadFromFile("../assets/images/map.png");
@@ -113,13 +125,17 @@ int main()
 
     Player p("hero.png", 250, 250, 96.0, 96.0);
 
-    float CurrentFrame = 0;
-    Clock clock;
 
+    float currentFrame = 0;
+    Clock clock;
+    Clock gameTimeClock;//переменная игрового времени, будем здесь хранить время игры
+    int gameTime = 0;//объявили игровое время, инициализировали.
     while (window.isOpen())
     {
 
         float time = clock.getElapsedTime().asMicroseconds();
+
+        if (p.life) gameTime = gameTimeClock.getElapsedTime().asSeconds(); else  { view.move(0.8, 0); }//игровое время в секундах идёт вперед, пока жив игрок, перезагружать как time его не надо. оно не обновляет логику игры
         clock.restart();
         time = time / 800;
 
@@ -132,36 +148,36 @@ int main()
         }
 
         ///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
-        if (Keyboard::isKeyPressed(Keyboard::Left)) {
-            p.dir = 1; p.speed = 0.1;
-            CurrentFrame += 0.005*time;
-            if (CurrentFrame > 3) CurrentFrame -= 3;
-            p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 96, 96, 96));
+        if (p.life) {
+            if (Keyboard::isKeyPressed(Keyboard::Left)) {
+                p.dir = 1; p.speed = 0.1;
+                currentFrame += 0.005*time;
+                if (currentFrame > 3) currentFrame -= 3;
+                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 96, 96, 96));
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Right)) {
+                p.dir = 0; p.speed = 0.1;
+                currentFrame += 0.005*time;
+                if (currentFrame > 3) currentFrame -= 3;
+                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 192, 96, 96));
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Up)) {
+                p.dir = 3; p.speed = 0.1;
+                currentFrame += 0.005*time;
+                if (currentFrame > 3) currentFrame -= 3;
+                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 307, 96, 96));
+            }
+
+            if (Keyboard::isKeyPressed(Keyboard::Down)) {
+                p.dir = 2; p.speed = 0.1;
+                currentFrame += 0.005*time;
+                if (currentFrame > 3) currentFrame -= 3;
+                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
+            }
+            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
         }
-
-        if (Keyboard::isKeyPressed(Keyboard::Right)) {
-            p.dir = 0; p.speed = 0.1;
-            CurrentFrame += 0.005*time;
-            if (CurrentFrame > 3) CurrentFrame -= 3;
-            p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 192, 96, 96));
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::Up)) {
-            p.dir = 3; p.speed = 0.1;
-            CurrentFrame += 0.005*time;
-            if (CurrentFrame > 3) CurrentFrame -= 3;
-            p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 307, 96, 96));//
-        }
-
-        if (Keyboard::isKeyPressed(Keyboard::Down)) {
-            p.dir = 2; p.speed = 0.1;
-            CurrentFrame += 0.005*time;
-            if (CurrentFrame > 3) CurrentFrame -= 3;
-            p.sprite.setTextureRect(IntRect(96 * int(CurrentFrame), 0, 96, 96));
-        }
-
-
-        getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
         p.update(time);
 
         window.setView(view);
@@ -174,14 +190,16 @@ int main()
                 if (TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(0, 0, 32, 32));
                 if (TileMap[i][j] == 's')  s_map.setTextureRect(IntRect(32, 0, 32, 32));
                 if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
-
+                if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));//добавили цветок
+                if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));//и сердечко
                 s_map.setPosition(j * 32, i * 32);
 
                 window.draw(s_map);
             }
-        std::ostringstream playerScoreString;    // объявили переменную
-        playerScoreString << p.playerScore;		//занесли в нее число очков, то есть формируем строку
-        text.setString(L"Собрано камней:" + playerScoreString.str());//задаем строку тексту и вызываем сформированную выше строку методом .str()
+        std::ostringstream playerHealthString, gameTimeString;    // объявили переменную здоровья и времени
+        playerHealthString << p.health; gameTimeString << gameTime;		//формируем строку
+        text.setString(L"Здоровье: " + playerHealthString.str() + L"\nВремя игры: " + gameTimeString.str());//задаем строку тексту и вызываем сформированную выше строку методом .str()
+
         text.setPosition(view.getCenter().x - 165, view.getCenter().y - 200);//задаем позицию текста, отступая от центра камеры
         window.draw(text);//рисую этот текст
 
