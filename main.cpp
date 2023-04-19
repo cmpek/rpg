6b5327e8161f765bf3d8fe419132973a955805d7
Shvedov Andrey <shvedov.ag@lsrgroup.ru>
2023-04-19 10:13:08 +0300

    lesson20

--- main.cpp
+++ main.cpp
@@ -10,65 +10,83 @@ using namespace sf;
 
 ////////////////////////////////////////////////////КЛАСС ИГРОКА////////////////////////
 class Player {
-    /* это задел на следующие уроки,прошу не обращать внимания)
-private: float w, h, dx, dy, x, y, speed;
-         int dir, playerScore, health;
-         bool life;
-         */
 
 public:
-    float w, h, dx, dy,x,y, speed;
+    float w,h,dx,dy,x,y,speed;
     int dir, playerScore, health;
-    bool life, isMove,isSelect;//добавили переменные состояния движения и выбора объекта
+    bool life, isMove,isSelect,onGround;//добавили переменные состояния нахождения на земле
+    enum { left,right,up,down,jump,stay } state;//добавляем тип перечисления - состояние объекта
     String File;
     Image image;
     Texture texture;
     Sprite sprite;
     Player(String F, float X, float Y, float W, float H){
+
         dir = 0; speed = 0; playerScore = 0; health = 100; dx = 0; dy = 0;
-        life = true; isMove = false; isSelect = false;
+        life = true; isMove = false; isSelect = false; onGround = false;
         File = F;
         w = W; h = H;
         image.loadFromFile("../assets/images/" + File);
-        image.createMaskFromColor(Color(0, 0, 255));
+        image.createMaskFromColor(Color(41, 33, 59));
         texture.loadFromImage(image);
         sprite.setTexture(texture);
 
         x = X; y = Y;
-        sprite.setTextureRect(IntRect(0, 0, w, h));
+        sprite.setTextureRect(IntRect(0, 134, w, h));
         sprite.setOrigin(w / 2, h / 2);
+    }
+    void control(){
+        if (Keyboard::isKeyPressed(Keyboard::Left)) {
+            state = left;
+            speed = 0.1;
+            //currentFrame += 0.005*time;
+            //if (currentFrame > 3) currentFrame -= 3;
+            //p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 135, 96, 54));
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Right)) {
+            state = right;
+            speed = 0.1;
+            //	currentFrame += 0.005*time;
+            //	if (currentFrame > 3) currentFrame -= 3;
+            //	p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 232, 96, 54));
+        }
+
+        if ((Keyboard::isKeyPressed(Keyboard::Up)) && (onGround)) {
+            state = jump; dy = -0.4; onGround = false;//то состояние равно прыжок,прыгнули и сообщили, что мы не на земле
+            //currentFrame += 0.005*time;
+            //if (currentFrame > 3) currentFrame -= 3;
+            //p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 307, 96, 96));
+        }
+
+        if (Keyboard::isKeyPressed(Keyboard::Down)) {
+            state = down;
+            speed = 0.1;
+
+            //currentFrame += 0.005*time;
+            //if (currentFrame > 3) currentFrame -= 3;
+            //p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
+        }
     }
     void update(float time)
     {
-        switch (dir)
+        control();//функция управления персонажем
+        switch (state)//тут делаются различные действия в зависимости от состояния
         {
-            case 0: dx = speed; dy = 0; break;
-            case 1: dx = -speed; dy = 0; break;
-            case 2: dx = 0; dy = speed; break;
-            case 3: dx = 0; dy = -speed; break;
+            case right: dx = speed;break;//состояние идти вправо
+            case left: dx = -speed;break;//состояние идти влево
+            case up: break;//будет состояние поднятия наверх (например по лестнице)
+            case down: break;//будет состояние во время спуска персонажа (например по лестнице)
+            case jump: break;//здесь может быть вызов анимации
+            case stay: break;//и здесь тоже
         }
-
         x += dx*time;
+        checkCollisionWithMap(dx, 0);//обрабатываем столкновение по Х
         y += dy*time;
+        checkCollisionWithMap(0, dy);//обрабатываем столкновение по Y
         if (!isMove) speed = 0;
-        sprite.setPosition(x, y);
-        interactionWithMap();
-        if (health <= 0){ life = false; }
-
-    }
-
-    float getWidth(){//получить ширину объека
-        return w;
-    }
-    void setWidth(float width){//установить ширину объекта
-        w = width;
-    }
-
-    float getHeight(){//взять ширину объекта
-        return h;
-    }
-    void setHeight(float height){//задать ширину объекта
-        h = height;
+        sprite.setPosition(x+w/2, y+h/2); //задаем позицию спрайта в место его центра
+        if (health <= 0){life = false;}
+        dy = dy + 0.0015*time;//делаем притяжение к земле
     }
 
     float getplayercoordinateX(){
@@ -78,55 +96,20 @@ public:
         return y;
     }
 
-
-    void moveToCursor(Window window){
-
-    }
-
-    void interactionWithMap()
+    void checkCollisionWithMap(float Dx, float Dy)//ф ция проверки столкновений с картой
     {
-
-        for (int i = y / 32; i < (y + h) / 32; i++)
+        for (int i = y / 32; i < (y + h) / 32; i++)//проходимся по элементам карты
             for (int j = x / 32; j<(x + w) / 32; j++)
             {
-                if (TileMap[i][j] == '0')
+                if (TileMap[i][j] == '0')//если элемент наш тайлик земли? то
                 {
-                    if (dy>0)
-                    {
-                        y = i * 32 - h;
-                    }
-                    if (dy<0)
-                    {
-                        y = i * 32 + 32;
-                    }
-                    if (dx>0)
-                    {
-                        x = j * 32 - w;
-                    }
-                    if (dx < 0)
-                    {
-                        x = j * 32 + 32;
-                    }
+                    if (Dy>0){ y = i * 32 - h;  dy = 0; onGround = true; }//по Y вниз=>идем в пол(стоим на месте) или падаем. В этот момент надо вытолкнуть персонажа и поставить его на землю, при этом говорим что мы на земле тем самым снова можем прыгать
+                    if (Dy<0){y = i * 32 + 32;  dy = 0;}//столкновение с верхними краями карты(может и не пригодиться)
+                    if (Dx>0){x = j * 32 - w; }//с правым краем карты
+                    if (Dx<0){x = j * 32 + 32;}// с левым краем карты
                 }
-
-                if (TileMap[i][j] == 's') {
-                    playerScore++;
-                    TileMap[i][j] = ' ';
-                }
-
-                if (TileMap[i][j] == 'f') {
-                    health -= 40;
-                    TileMap[i][j] = ' ';
-                }
-
-                if (TileMap[i][j] == 'h') {
-                    health += 20;
-                    TileMap[i][j] = ' ';
-                }
-
             }
     }
-
 };
 
 class SpriteManager{//это задел на следующие уроки,прошу не обращать внимания на эти изменения)
@@ -152,7 +135,7 @@ int main()
 {
 
 
-    RenderWindow window(VideoMode(640, 480), "Lesson 19. kychka-pc.ru");
+    RenderWindow window(VideoMode(640, 480), "Lesson 20. kychka-pc.ru");
     view.reset(FloatRect(0, 0, 640, 480));
 
     Font font;
@@ -180,7 +163,7 @@ int main()
 
     SpriteManager playerSprite("hero.png", "Hero");//это задел на следующие уроки,прошу не обращать внимания)
 
-    Player p("heroForRotate.png", 250, 250, 136, 74);
+    Player p("hero.png", 250, 500, 96, 54);
 
     float currentFrame = 0;
     Clock clock;
@@ -225,7 +208,11 @@ int main()
                         p.sprite.setColor(Color::White);//возвращаем обычный цвет спрайту
                         tempX = pos.x;//забираем координату нажатия курсора Х
                         tempY = pos.y;//и Y
-
+                        float dX = pos.x - p.x;//вектор , колинеарный прямой, которая пересекает спрайт и курсор
+                        float dY = pos.y - p.y;//он же, координата y
+                        float rotation = (atan2(dY, dX)) * 180 / 3.14159265;//получаем угол в радианах и переводим его в градусы
+                        std::cout << rotation << "\n";//смотрим на градусы в консольке
+                        p.sprite.setRotation(rotation);//поворачиваем спрайт на эти градусы
                     }
         }
 
@@ -244,44 +231,11 @@ int main()
 
 
 
-        float dX = pos.x - p.x;//вектор , колинеарный прямой, которая пересекает спрайт и курсор
-        float dY = pos.y - p.y;//он же, координата y
-        float rotation = (atan2(dY, dX)) * 180 / 3.14159265;//получаем угол в радианах и переводим его в градусы
-        std::cout << rotation << "\n";//смотрим на градусы в консольке
-        p.sprite.setRotation(rotation);//поворачиваем спрайт на эти градусы
+
 
 
         ///////////////////////////////////////////Управление персонажем с анимацией////////////////////////////////////////////////////////////////////////
-        if (p.life) {
-            if (Keyboard::isKeyPressed(Keyboard::Left)) {
-                p.dir = 1; p.speed = 0.1;
-                currentFrame += 0.005*time;
-                if (currentFrame > 3) currentFrame -= 3;
-                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 96, 96, 96));
-            }
-
-            if (Keyboard::isKeyPressed(Keyboard::Right)) {
-                p.dir = 0; p.speed = 0.1;
-                currentFrame += 0.005*time;
-                if (currentFrame > 3) currentFrame -= 3;
-                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 192, 96, 96));
-            }
-
-            if (Keyboard::isKeyPressed(Keyboard::Up)) {
-                p.dir = 3; p.speed = 0.1;
-                currentFrame += 0.005*time;
-                if (currentFrame > 3) currentFrame -= 3;
-                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 307, 96, 96));
-            }
-
-            if (Keyboard::isKeyPressed(Keyboard::Down)) {
-                p.dir = 2; p.speed = 0.1;
-
-                currentFrame += 0.005*time;
-                if (currentFrame > 3) currentFrame -= 3;
-                p.sprite.setTextureRect(IntRect(96 * int(currentFrame), 0, 96, 96));
-            }
-            getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+        if (p.life) {getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
         }
 
 
@@ -291,7 +245,6 @@ int main()
         window.setView(view);
         window.clear();
 
-        window.getSystemHandle();
 
         for (int i = 0; i < HEIGHT_MAP; i++)
             for (int j = 0; j < WIDTH_MAP; j++)
